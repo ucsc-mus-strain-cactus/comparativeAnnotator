32eec8f8fb3e79a9dca44273c66f4288720bca35
ifiddes <ian.t.fiddes@gmail.com>
2015-08-26 15:50:36 -0700

    reworked consensus script to actually generate a consensus. Renamed tiers. Added a script to do hierarchical clustering of classifiers. Next: integrate the plots in consensus_more.txt and do them for every genome/biotype combination. Make sure to denote when aug/tm tied.

---
+++ scripts/clustering_classifiers.py
@@ -0,0 +1,184 @@
+# analyzing the classifiers failed most commonly in the basic set, then comparing this to the comprehensive set
+
+import os
+import sys
+import itertools
+import sqlite3 as sql
+from lib.psl_lib import removeAlignmentNumber, removeAugustusAlignmentNumber
+from lib.sqlite_lib import attachDatabase
+sys.path.append("/hive/users/ifiddes/comparativeAnnotator")
+import matplotlib
+import matplotlib.pyplot as plt
+matplotlib.use('Agg')
+import seaborn as sns
+
+def attach_databases(comp_ann_path):
+    """
+    Attaches all of the databases. Expects comp_ann_path to be the path that comparativeAnnotator wrote to.
+    """
+    con = sql.connect(os.path.join(comp_ann_path, "classify.db"))
+    cur = con.cursor()
+    attachDatabase(con, os.path.join(comp_ann_path, "augustusClassify.db"), "augustus")
+    attachDatabase(con, os.path.join(comp_ann_path, "attributes.db"), "attributes")
+    return con, cur
+
+comp_ann_dir = "/hive/groups/recon/projs/mus_strain_cactus/pipeline_data/comparative/1504/comparativeAnnotation/2015-08-10_Augustus"
+con, cur = attach_databases(comp_ann_dir)
+
+tm_fields = ["CodingInsertions", "CodingDeletions", "StartOutOfFrame", "FrameShift", 
+                      "AlignmentAbutsLeft", "AlignmentAbutsRight", "AlignmentPartialMap", "BadFrame", "BeginStart",
+                      "CdsGap", "CdsMult3Gap", "UtrGap", "UnknownGap", "CdsUnknownSplice", "UtrUnknownSplice", 
+                      "EndStop", "InFrameStop", "ShortCds", "UnknownBases", "AlignmentAbutsUnknownBases"]
+
+
+aug_fields = ['AugustusParalogy', 'AugustusExonGain', 'AugustusExonLoss', 'AugustusNotSameStrand', 
+              'AugustusNotSameStartStop', 'AugustusNotSimilarTerminalExonBoundaries', 
+              'AugustusNotSimilarInternalExonBoundaries']
+
+
+tm_fail_counts = {}
+for tm_classifier in tm_fields:
+    cmd = """SELECT main.'C57B6NJ'.'AlignmentId' FROM main.'C57B6NJ' WHERE main.'C57B6NJ'.'{}' = 1""".format(tm_classifier)
+    r = cur.execute(cmd).fetchall()
+    tm_fail_counts[tm_classifier] = len(r)
+
+
+aug_fail_counts = {}
+for aug_classifier in aug_fields:
+    cmd = """SELECT augustus.'C57B6NJ'.'AlignmentId' FROM augustus.'C57B6NJ' WHERE augustus.'C57B6NJ'.'{}' = 1""".format(aug_classifier)
+    r = cur.execute(cmd).fetchall()
+    aug_fail_counts[aug_classifier] = len(r)
+
+
+
+
+# decided to do heiarchaical clustering in R instead of dealing with matplotlib
+
+#sqlite3 command to dump to text: select AlignmentId,CodingInsertions,CodingDeletions,StartOutOfFrame,FrameShift,AlignmentAbutsLeft,AlignmentAbutsRight,AlignmentPartialMap,BadFrame,BeginStart,CdsGap,CdsMult3Gap,UtrGap,UnknownGap,CdsUnknownSplice,UtrUnknownSplice,EndStop,InFrameStop,ShortCds,UnknownBases,AlignmentAbutsUnknownBases from C57B6NJ;
+
+# R code
+
+data <- read.csv("~/mus_strain_data/pipeline_data/comparative/1504/comparativeAnnotation/2015-08-10_Augustus/C57B6NJ.transMap.csv", row.names=1, header=T, na.strings="")
+# need to convert NA to 0
+data[is.na(data)] <- 0
+mat <- sapply(as.data.frame(data), as.logical)
+mat.t <- t(mat)
+library(stats)
+d <- dist(mat.t, method="binary")
+hc <- hclust(d, method="ward")
+pdf("transMap_clustered_classifiers.pdf")
+plot(hc)
+dev.off()
+
+pdf("transMap_categories.pdf")
+data.transactions <- as(mat, "transactions")
+itemFrequencyPlot(data.transactions, support=0.1, cex.names=0.7)
+dev.off()
+
+rules <- apriori(data.transactions, parameter=list(support=0.01, confidence=0.7))
+# i have no fucking clue how to interpret this, but at least the frequency plot is nice
+
+
+# now I want to re-run this on just Basic set protein_coding. I can't just dump to csv this time.
+from scripts.coverage_identity_ok_plots import *
+from scripts.consensus import *
+attrs = "/cluster/home/ifiddes/mus_strain_data/pipeline_data/comparative/1504/transMap/2015-05-28/data/wgEncodeGencodeAttrsVM4.tsv"
+coding_ids = get_all_ids(attrs, biotype="protein_coding")
+basic_ids = {x.split()[0] for x in open("/cluster/home/ifiddes/mus_strain_data/pipeline_data/comparative/1504/transMap/2015-05-28/data/wgEncodeGencodeBasicVM4.gp")}
+basic_coding = {x for x in basic_ids if x in coding_ids}
+tm_cmd = """SELECT AlignmentId,{} FROM main.'C57B6NJ'""".format(",".join(tm_fields))
+r = cur.execute(tm_cmd).fetchall()
+r_coding = [x for x in r if removeAlignmentNumber(x[0]) in basic_coding]
+with open("transmap_coding_only.csv", "w") as outf:
+    outf.write("AlignmentId," + ",".join(tm_fields) + "\n")
+    for x in r_coding:
+        outf.write(",".join(map(str, x)) + "\n")
+
+
+data <- read.csv("/hive/users/ifiddes/comparativeAnnotator/transmap_coding_only.csv", row.names=1, header=T, na.strings="None")
+data[is.na(data)] <- 0
+mat <- sapply(as.data.frame(data), as.logical)
+mat.t <- t(mat)
+library(stats)
+d <- dist(mat.t, method="binary")
+hc <- hclust(d, method="ward")
+pdf("transMap_clustered_classifiers_coding_basic.pdf")
+plot(hc)
+dev.off()
+pdf("transMap_categories_coding_basic.pdf")
+data.transactions <- as(mat, "transactions")
+itemFrequencyPlot(data.transactions, support=0.1, cex.names=0.7)
+dev.off()
+
+
+data <- read.csv("~/mus_strain_data/pipeline_data/comparative/1504/comparativeAnnotation/2015-08-10_Augustus/C57B6NJ.augustus.csv", row.names=1, header=T, na.strings="")
+# need to convert NA to 0
+data[is.na(data)] <- 0
+mat <- sapply(as.data.frame(data), as.logical)
+mat.t <- t(mat)
+library(stats)
+d <- dist(mat.t, method="binary")
+hc <- hclust(d, method="ward")
+pdf("augustus_clustered_classifiers.pdf")
+plot(hc)
+dev.off()
+
+pdf("augustus_categories.pdf")
+data.transactions <- as(mat, "transactions")
+itemFrequencyPlot(data.transactions, support=0.1, cex.names=0.7)
+dev.off()
+
+
+# now I want the complement - transcripts in comprehensive that are not in basic
+from scripts.coverage_identity_ok_plots import *
+from scripts.consensus import *
+attrs = "/cluster/home/ifiddes/mus_strain_data/pipeline_data/comparative/1504/transMap/2015-05-28/data/wgEncodeGencodeAttrsVM4.tsv"
+coding_ids = get_all_ids(attrs, biotype="protein_coding")
+basic_ids = {x.split()[0] for x in open("/cluster/home/ifiddes/mus_strain_data/pipeline_data/comparative/1504/transMap/2015-05-28/data/wgEncodeGencodeBasicVM4.gp")}
+basic_coding = {x for x in basic_ids if x in coding_ids}
+comp_specific_ids = coding_ids - basic_ids
+tm_cmd = """SELECT AlignmentId,{} FROM main.'C57B6NJ'""".format(",".join(tm_fields))
+r = cur.execute(tm_cmd).fetchall()
+r_coding = [x for x in r if removeAlignmentNumber(x[0]) in comp_specific_ids]
+with open("transmap_comp_specific.csv", "w") as outf:
+    outf.write("AlignmentId," + ",".join(tm_fields) + "\n")
+    for x in r_coding:
+        outf.write(",".join(map(str, x)) + "\n")
+
+
+data <- read.csv("/hive/users/ifiddes/comparativeAnnotator/transmap_comp_specific.csv", row.names=1, header=T, na.strings="None")
+# need to convert NA to 0
+data[is.na(data)] <- 0
+mat <- sapply(as.data.frame(data), as.logical)
+mat.t <- t(mat)
+library(stats)
+d <- dist(mat.t, method="binary")
+hc <- hclust(d, method="ward")
+pdf("transMap_comp_only_classifiers.pdf")
+plot(hc)
+dev.off()
+
+pdf("transMap_comp_only_categories.pdf")
+data.transactions <- as(mat, "transactions")
+itemFrequencyPlot(data.transactions, support=0.1, cex.names=0.7)
+dev.off()
+
+# not sure what I want to use this for
+tm_grouped_fields = [["CodingInsertions", "CodingDeletions"], ["CodingMult3Deletions", "CodingMult3Insertions"], "StartOutOfFrame", "FrameShift",
+                     ["AlignmentAbutsLeft", "AlignmentAbutsRight"], "AlignmentPartialMap", "BadFrame", ["BeginStart", "EndStop"], 
+                     ["CdsGap", "CdsMult3Gap"], "UtrGap", "UnknownGap", ["CdsUnknownSplice", "UtrUnknownSplice"], "InFrameStop", "ShortCds", 
+                     ["UnknownBases", "AlignmentAbutsUnknownBases"]]
+
+
+# how many genes are present in comp that are not present in basic?
+attr_map = {x.split()[3]: x.split()[0] for x in open(attrs)}
+comp_genes = {attr_map[x] for x in attr_map if x not in basic_ids}
+# 19,909. What biotypes do we have here?
+biotype_map = {x.split()[0]: x.split()[2] for x in open(attrs)}
+from collections import Counter
+biotypes = Counter()
+for g in comp_genes:
+    biotypes[biotype_map[g]] += 1
+# Counter({'protein_coding': 10545, 'processed_pseudogene': 5421, 'unprocessed_pseudogene': 2042, 'lincRNA': 829, 'processed_transcript': 370, 'pseudogene': 179, 'transcribed_processed_pseudogene': 139, 'transcribed_unprocessed_pseudogene': 128, 'IG_V_pseudogene': 69, 'IG_V_gene': 40, 'TEC': 33, 'TR_V_gene': 23, 'antisense': 21, 'unitary_pseudogene': 15, 'TR_V_pseudogene': 14, 'polymorphic_pseudogene': 9, 'sense_intronic': 8, 'IG_C_gene': 6, 'IG_D_pseudogene': 4, 'sense_overlapping': 3, 'TR_J_gene': 2, 'IG_C_pseudogene': 1, 'translated_processed_pseudogene': 1, 'geneType': 1, 'IG_J_gene': 1, 'TR_J_pseudogene': 1, '3prime_overlapping_ncrna': 1, 'translated_unprocessed_pseudogene': 1, 'IG_D_gene': 1, 'TR_C_gene': 1})
+# still over half coding, but highly enriched for pseudogenes as expected
+
--- scripts/consensus.py
+++ scripts/consensus.py
@@ -2,9 +2,11 @@ import os
 import itertools
 import argparse
 import sqlite3 as sql
+from collections import defaultdict
 from lib.psl_lib import removeAlignmentNumber, removeAugustusAlignmentNumber
 from lib.sqlite_lib import attachDatabase
 from lib.general_lib import mkdir_p
+from sonLib.bioio import system
 
 
 def parse_args():
@@ -19,19 +21,39 @@ def parse_args():
     return parser.parse_args()
 
 
-def transmap_ok(cur, genome):
+# these classifiers define OK for coding transcripts
+tm_coding_classifiers = ["CodingInsertions", "CodingDeletions", "StartOutOfFrame", "FrameShift", 
+                         "AlignmentAbutsLeft", "AlignmentAbutsRight", "AlignmentPartialMap", "BadFrame", "BeginStart",
+                         "CdsGap", "CdsMult3Gap", "UtrGap", "UnknownGap", "CdsUnknownSplice", "UtrUnknownSplice", 
+                         "EndStop", "InFrameStop", "ShortCds", "UnknownBases", "AlignmentAbutsUnknownBases"]
+
+# these classifiers define OK for non-coding transcripts
+tm_noncoding_classifiers = ["AlignmentAbutsLeft", "AlignmentAbutsRight", "AlignmentPartialMap", "UtrUnknownSplice",
+                            "UtrGap", "UnknownGap", "UnknownBases", "AlignmentAbutsUnknownBases"]
+
+
+def skip_header(path):
+    f_h = open(path)
+    _ = f_h.next()
+    return f_h
+
+
+def get_all_biotypes(attr_path):
+    """
+    Returns all biotypes in the attribute database.
+    """
+    return {x.split()[4] for x in skip_header(attr_path)}
+
+
+def transmap_ok(cur, genome, classify_fields):
     """
     Finds all aIds which are 'OK' based on the classifyFields below
     """
-    classifyFields = ["CodingInsertions", "CodingDeletions", "CodingDeletions", "StartOutOfFrame", "FrameShift", 
-                      "AlignmentAbutsLeft", "AlignmentAbutsRight", "AlignmentPartialMap", "BadFrame", "BeginStart",
-                      "CdsGap", "CdsMult3Gap", "UtrGap", "UnknownGap", "CdsUnknownSplice", "UtrUnknownSplice", 
-                      "EndStop", "InFrameStop", "ShortCds", "UnknownBases", "AlignmentAbutsUnknownBases"]
     cmd = """SELECT main.'{0}'.'AlignmentId' FROM main.'{0}' WHERE (""".format(genome)
-    for col in classifyFields[:-1]:
+    for col in classify_fields[:-1]:
         cmd += " main.'{}'.'{}' = ? {}".format(genome, col, "AND")
-    cmd += " main.'{}'.'{}' = ?)".format(genome, classifyFields[-1])
-    vals = [0] * len(classifyFields)
+    cmd += " main.'{}'.'{}' = ?)".format(genome, classify_fields[-1])
+    vals = [0] * len(classify_fields)
     return {x[0] for x in cur.execute(cmd, vals).fetchall()}
 
 
@@ -50,11 +72,11 @@ def augustus_ok(cur, genome):
     return {x[0] for x in cur.execute(cmd, vals).fetchall()}
 
 
-def get_all_ok(cur, genome):
+def get_all_ok(cur, genome, tm_classifiers):
     """
     Adapter function to return the combined sets of ok from augustus and transmap
     """
-    return augustus_ok(cur, genome) | transmap_ok(cur, genome)
+    return augustus_ok(cur, genome) | transmap_ok(cur, genome, tm_classifiers)
 
 
 def get_all_ids(attr_path, biotype=None):
@@ -62,9 +84,9 @@ def get_all_ids(attr_path, biotype=None):
     returns the set of ensembl IDs in the entire Gencode database pulled from the attribute
     """
     if biotype is None:
-        return {x.split()[3] for x in open(attr_path)}
+        return {x.split()[3] for x in skip_header(attr_path)}
     else:
-        return {x.split()[3] for x in open(attr_path) if x.split()[4] == biotype}
+        return {x.split()[3] for x in skip_header(attr_path) if x.split()[4] == biotype}
 
 
 def get_reverse_name_map(cur, genome, ids):
@@ -80,7 +102,8 @@ def get_reverse_name_map(cur, genome, ids):
     for aln_id in itertools.chain(aug_r, tm_r):
         aln_id = aln_id[0]
         ens_id = removeAlignmentNumber(removeAugustusAlignmentNumber(aln_id))
-        reverse_name_map[ens_id].append(aln_id)
+        if ens_id in ids:
+            reverse_name_map[ens_id].append(aln_id)
     return reverse_name_map
 
 
@@ -189,7 +212,8 @@ def bin_candidates(winner_id, aug_alts, tm_alts, bins, binned_transcripts):
 
 
 def bin_transcripts(reverse_name_map, stats_dict, ok_ids, ens_ids):
-    binned_transcripts = {"T1": [], "T2A": [], "T2T": [], "T3": [], "T4A": [], "T4T": [], "fail": [], "discarded": []}
+    binned_transcripts = {"bestOk": [], "augAltOk": [], "tmAltOk": [], "bestNotOk": [], "augAltNotOk": [], 
+                          "tmAltNotOk": [], "fail": [], "discarded": []}
     for ens_id in ens_ids:
         aln_ids = reverse_name_map[ens_id]
         t1_candidates, t3_candidates, discarded = find_t1_t3_candidates(stats_dict, aln_ids, ok_ids,
@@ -199,10 +223,10 @@ def bin_transcripts(reverse_name_map, stats_dict, ok_ids, ens_ids):
             binned_transcripts["fail"].append(ens_id)
         elif len(t1_candidates) > 0:
             t1_id, aug_t2, tm_t2 = analyze_candidates(t1_candidates)
-            bin_candidates(t1_id, aug_t2, tm_t2, ["T1", "T2A", "T2T"], binned_transcripts)
+            bin_candidates(t1_id, aug_t2, tm_t2, ["bestOk", "augAltOk", "tmAltOk"], binned_transcripts)
         else:
             t3_id, aug_t4, tm_t4 = analyze_candidates(t3_candidates)
-            bin_candidates(t3_id, aug_t4, tm_t4, ["T3", "T4A", "T4T"], binned_transcripts)
+            bin_candidates(t3_id, aug_t4, tm_t4, ["bestNotOk", "augAltNotOk", "tmAltNotOk"], binned_transcripts)
     return binned_transcripts
 
 
@@ -210,51 +234,118 @@ def load_gps(gp_paths):
     return {l.split()[0]: l for p in gp_paths for l in open(p)}
 
 
-def write_gps(binned_transcripts, gps, out_dir, genome, filter_set=None):
-    p = os.path.join(out_dir, genome)
+def fix_gene_pred(gp, gene_map):
+    """
+    These genePreds have a few problems. First, the alignment numbers must be removed. Second, we want to fix
+    the name2 field to be the gene name. Finally, we want to sort the whole thing by genomic coordinates.
+    """
+    gp = sorted([x.split("\t") for x in gp], key=lambda x: [x[1], x[3]])
+    fixed = []
+    for i, x in enumerate(gp):
+        tx_id = removeAlignmentNumber(removeAugustusAlignmentNumber(x[0]))
+        x[0] = tx_id
+        gene_id = gene_map[tx_id]
+        x[10] = str(i)
+        x[11] = gene_id
+        fixed.append(x)
+    return ["\t".join(x) for x in fixed]
+
+
+def write_gps(binned_transcripts, gps, out_dir, genome, biotype, gene_map):
+    p = os.path.join(out_dir, genome, biotype)
     mkdir_p(p)
-    for b in ["T1", "T2A", "T2T", "T3", "T4A", "T4T"]:
-        with open(os.path.join(p, genome + "_Tier" + b + ".gp"), "w") as outf:
-            for aln_id in binned_transcripts[b]:
-                gp = gps[aln_id]
-                if filter_set is None:
-                    outf.write(gp)
-                elif removeAlignmentNumber(removeAugustusAlignmentNumber(aln_id)) in filter_set:
-                    outf.write(gp)
+    for b in ["bestOk", "augAltOk", "tmAltOk", "bestNotOk", "augAltNotOk", "tmAltNotOk"]:
+        gp = [gps[aln_id] for aln_id in binned_transcripts[b]]
+        fixed_gp = fix_gene_pred(gp, gene_map)
+        with open(os.path.join(p, genome + "_" + b + ".gp"), "w") as outf:
+            for x in fixed_gp:
+                outf.write(x)
     for b in ["fail", "discarded"]:
         with open(os.path.join(p, genome + "_" + b + ".txt"), "w") as outf:
             for aln_id in binned_transcripts[b]:
-                if filter_set is None:
-                    outf.write(aln_id + "\n")
-                elif removeAlignmentNumber(removeAugustusAlignmentNumber(aln_id)) in filter_set:
-                    outf.write(aln_id + "\n")
+                outf.write(aln_id + "\n")
 
 
-def driver_function(genome, tm_gp, aug_gp, con, cur, ens_ids, coding_ids, stats_dir, out_dir):
-    reverse_name_map = get_reverse_name_map(cur, genome, ens_ids)
-    ok_ids = get_all_ok(cur, genome)
-    stats_dict = merge_stats(cur, stats_dir, genome)
-    binned_transcripts = bin_transcripts(reverse_name_map, stats_dict, ok_ids, ens_ids)
-    gps = load_gps([tm_gp, aug_gp])
-    all_path = os.path.join(out_dir, "all_transcripts")
-    write_gps(binned_transcripts, gps, all_path, genome)
-    coding_path = os.path.join(out_dir, "coding_transcripts")
-    write_gps(binned_transcripts, gps, coding_path, genome, filter_set=coding_ids) 
+def find_not_ok_genes(gene_map, not_ok_gps):
+    """
+    Returns the set of (not failed/discarded) genes for which we have no OK transcripts.
+    """
+    return {gene_map[removeAlignmentNumber(removeAugustusAlignmentNumber(x))] for x in not_ok_gps}
+
+
+def get_gene_biotype_map(attr_path):
+    """
+    Returns a dictionary mapping all gene IDs to their respective biotypes
+    """
+    return {x.split()[0]: x.split()[2] for x in skip_header(attr_path)}
+
+
+def get_gene_map(attr_path):
+    """
+    Returns a dictionary mapping all transcript IDs to their respective gene IDs
+    """
+    return {x.split()[3]: x.split()[0] for x in skip_header(attr_path)}
+
+
+def consensus_gene_set(binned_transcripts, stats_dict, gps, gene_map, gene_biotype_map, gene_cov_cutoff=0.20):
+    """
+    Builds the consensus gene set. For each transcript that has a best OK/not OK (passes coverage filter),
+    report it. For the remaining transcripts, determine the set of genes they come from. Find anything above
+    gene_cov_cutoff to be the one transcript to best represent this gene.
+    """
+    consensus = []
+    for b in ["bestOk", "bestNotOk"]:
+        for aln_id in binned_transcripts[b]:
+            consensus.append(gps[aln_id])
+    discarded_genes = defaultdict(list)
+    for x in binned_transcripts["discarded"]:
+        discarded_genes[gene_map[removeAlignmentNumber(removeAugustusAlignmentNumber(x))]].append(x)
+    for gene, transcripts in discarded_genes.iteritems():
+        stats = [stats_dict[x] for x in transcripts if stats_dict[x][1] != None and stats_dict[x][2] > gene_cov_cutoff]
+        if len(stats) > 0:
+            best = find_best_aln(stats)
+            consensus.append(gps[best])
+    return consensus
+
+
+def write_consensus(consensus, gene_map, consensus_path):
+    consensus = fix_gene_pred(consensus, gene_map)
+    with open(consensus_path, "w") as outf:
+        for x in consensus:
+            outf.write(x)
 
 
 def main():
     args = parse_args()
     con, cur = attach_databases(args.compAnnPath)
-    ens_ids = get_all_ids(args.attributePath)
-    coding_ids = get_all_ids(args.attributePath, biotype="protein_coding")
+    biotypes = get_all_biotypes(args.attributePath)
+    gene_map = get_gene_map(args.attributePath)
+    gene_biotype_map = get_gene_biotype_map(args.attributePath)
     sorted_genomes = sorted(args.genomes)
     sorted_tm_gps = sorted(args.tmGps)
     sorted_aug_gps = sorted(args.augGps)
+    consensus_base_path = os.path.join(args.outDir, "geneSets")
+    mkdir_p(consensus_base_path)
+    raw_bins_base_path = os.path.join(args.outDir, "binnedTranscripts")
+    mkdir_p(raw_bins_base_path)
     for genome, tm_gp, aug_gp in itertools.izip(sorted_genomes, sorted_tm_gps, sorted_aug_gps):
+        consensus = []
         assert genome in tm_gp and genome in aug_gp
-        driver_function(genome, tm_gp, aug_gp, con, cur, ens_ids, coding_ids, args.statsDir,
-                        args.outDir)
-
+        coding_ok = get_all_ok(cur, genome, tm_coding_classifiers)
+        noncoding_ok = get_all_ok(cur, genome, tm_noncoding_classifiers)
+        gps = load_gps([tm_gp, aug_gp])
+        for biotype in biotypes:
+            ens_ids = get_all_ids(args.attributePath, biotype=biotype)
+            reverse_name_map = get_reverse_name_map(cur, genome, ens_ids)
+            stats_dict = merge_stats(cur, args.statsDir, genome)
+            if biotype == "protein_coding": 
+                binned_transcripts = bin_transcripts(reverse_name_map, stats_dict, coding_ok, ens_ids)
+            else:
+                binned_transcripts = bin_transcripts(reverse_name_map, stats_dict, noncoding_ok, ens_ids)
+            consensus.extend(consensus_gene_set(binned_transcripts, stats_dict, gps, gene_map, gene_biotype_map))
+            write_gps(binned_transcripts, gps, raw_bins_base_path, genome, biotype, gene_map)
+        consensus_path = os.path.join(consensus_base_path, genome + "consensusGeneSet.gp")
+        write_consensus(consensus, gene_map, consensus_path)
 
 if __name__ == "__main__":
     main()
