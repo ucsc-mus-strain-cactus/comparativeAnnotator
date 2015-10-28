b5b8a2e604e111def1580f0449c9c58b33f788c2
ifiddes <ian.t.fiddes@gmail.com>
2015-10-28 07:40:22 -0700

    augustus bug

--- augustus/run_augustus.py
+++ augustus/run_augustus.py
@@ -30,7 +30,7 @@ hints_db_query = '''select source,typename,start,end,score,strand,frame,priority
              AND seqnr IN (SELECT seqnr FROM seqnames WHERE speciesid IN (SELECT speciesid FROM speciesnames WHERE speciesname="{genome}") AND seqname="{chrom}")
              AND start >= {start} AND end <= {stop} AND typeid=type'''
 
-augustus_base_cmd = ("augustus {fasta} --predictionStart=-{start} --predictionEnd=-{start} --extrinsicCfgFile={cfg} "
+augustus_cmd = ("augustus {fasta} --predictionStart=-{start} --predictionEnd=-{start} --extrinsicCfgFile={cfg} "
                      "--hintsfile={hints} --UTR=on --alternatives-from-evidence=0 --species=human "
                      "--allow_hinted_splicesites=atac --protein=0 --/augustus/verbosity=1 --softmasking=1 "
                      "--outfile=/dev/stdout")
