53b94e126a127afea58fe55a8e5d934d4dce3e09
ifiddes <ian.t.fiddes@gmail.com>
2015-10-28 11:56:50 -0700

    this method of aligning doesn't make sense because I don't have this information for transMap.

--- augustus/align_augustus.py
+++ augustus/align_augustus.py
@@ -1,21 +1,21 @@
 import os
 import argparse
-import itertools
 import pandas as pd
 from jobTree.scriptTree.target import Target
 from jobTree.scriptTree.stack import Stack
-from lib.seq_lib import get_sequence_dict, get_transcript_dict
-from lib.psl_lib import PslRow, remove_augustus_alignment_number, remove_alignment_number, strip_alignment_numbers
-from lib.general_lib import tokenize_stream, format_ratio
-from lib.sql_lib import ExclusiveSqlConnection
+from lib.psl_lib import PslRow, remove_augustus_alignment_number, remove_alignment_number
+from lib.general_lib import tokenize_stream
 from sonLib.bioio import fastaWrite, popenCatch, TempFileTree, catFiles
+from pyfaidx import Fasta
+from lib.general_lib import format_ratio
+from lib.sql_lib import ExclusiveSqlConnection
 
 
 def coverage(p_list):
     m = sum(x.matches for x in p_list)
     mi = sum(x.mismatches for x in p_list)
     rep = sum(x.repmatches for x in p_list)
-    # coverage can end up well above 100% if Augustus adds bases
+    # ident/cov can end up slightly above 1 due to adding floats
     cov = 100 * format_ratio(m + mi + rep, p_list[0].q_size)
     return min(cov, 100.0)
 
@@ -25,52 +25,46 @@ def identity(p_list):
     mi = sum(x.mismatches for x in p_list)
     rep = sum(x.repmatches for x in p_list)
     ins = sum(x.q_num_insert for x in p_list)
+    # ident/cov can end up slightly above 1 due to adding floats
     ident = 100 * format_ratio(m + rep, m + rep + mi + ins)
     return min(ident, 100.0)
 
 
-def seq_dict_paired_chunker(ref_dict, tx_dict, size=200):
-    target_it = iter(tx_dict)
-    for i in xrange(0, len(tx_dict), size):
-        yield [[tx_dict[aln_id], ref_dict[strip_alignment_numbers(aln_id)]] for aln_id in 
-               itertools.islice(target_it, size)]
+def chunker(seq, size):
+    return (seq[pos:pos + size] for pos in xrange(0, len(seq), size))
 
 
-def align(target, file_tree, chunk, ref_fasta, target_fasta):
-    ref_seq_dict = get_sequence_dict(ref_fasta)
-    target_seq_dict = get_sequence_dict(target_fasta)
+def align(target, target_fasta, chunk, ref_fasta, file_tree):
+    g_f = Fasta(target_fasta)
+    r_f = Fasta(ref_fasta)
     results = []
-    for tx, ref_tx in chunk:
-        tmp_results = [tx.name, remove_augustus_alignment_number(tx.name)]
-        for mode in ["mrna", "cds"]:
-            tmp_aug = os.path.join(target.getLocalTempDir(), "tmp_aug_{}".format(mode))
-            tmp_gencode = os.path.join(target.getLocalTempDir(), "tmp_gencode_{}".format(mode))
-            tx_fn = eval("tx.get_{}".format(mode))
-            tx_seq = tx_fn(target_seq_dict)
-            ref_fn = eval("ref_tx.get_{}".format(mode))
-            ref_seq = ref_fn(ref_seq_dict)
-            fastaWrite(tmp_aug, tx.name, tx_seq)
-            fastaWrite(tmp_gencode, tx.name, ref_seq)
-            r = popenCatch("blat {} {} -out=psl -noHead /dev/stdout".format(tmp_gencode, tmp_aug))
-            r = r.split("\n")[:-3]
-            if len(r) == 0:
-                tmp_results.extend(["0", "0"])
-            else:
-                p_list = [PslRow(x) for x in tokenize_stream(r)]
-                tmp_results.extend([identity(p_list), coverage(p_list)])
-        results.append(tmp_results)
+    for aug_aln_id in chunk:
+        aln_id = remove_augustus_alignment_number(aug_aln_id)
+        gencode_id = remove_alignment_number(aln_id)
+        gencode_seq = str(r_f[gencode_id])
+        aug_seq = str(g_f[aug_aln_id])
+        tmp_aug = os.path.join(target.getLocalTempDir(), "tmp_aug")
+        tmp_gencode = os.path.join(target.getLocalTempDir(), "tmp_gencode")
+        fastaWrite(tmp_aug, aug_aln_id, aug_seq)
+        fastaWrite(tmp_gencode, gencode_id, gencode_seq)
+        r = popenCatch("blat {} {} -out=psl -noHead /dev/stdout".format(tmp_gencode, tmp_aug))
+        r = r.split("\n")[:-3]
+        if len(r) == 0:
+            results.append([aug_aln_id, aln_id, "0", "0"])
+        else:
+            p_list = [PslRow(x) for x in tokenize_stream(r)]
+            results.append(map(str, [aug_aln_id, aln_id, identity(p_list), coverage(p_list)]))
     with open(file_tree.getTempFile(), "w") as outf:
         for x in results:
             outf.write("".join([",".join(x), "\n"]))
 
 
-def align_augustus(target, genome, ref_fasta, target_fasta, ref_gp, aug_gp, out_dir):
+def align_augustus(target, genome, ref_fasta, target_fasta, target_fasta_index, out_dir):
     file_tree = TempFileTree(target.getGlobalTempDir())
-    ref_dict = get_transcript_dict(ref_gp)
-    tx_dict = get_transcript_dict(aug_gp)
-    for chunk in seq_dict_paired_chunker(ref_dict, tx_dict, size=200):
-        target.addChildTargetFn(align, args=[file_tree, chunk, ref_fasta, target_fasta])
-    target.setFollowOnTargetFn(cat, args=[genome, file_tree, out_dir])
+    aug_aln_ids = [x.split()[0] for x in open(target_fasta_index)]
+    for chunk in chunker(aug_aln_ids, 200):
+        target.addChildTargetFn(align, args=[target_fasta, chunk, ref_fasta, file_tree])
+    target.setFollowOnTargetFn(cat, args=(genome, file_tree, out_dir))
 
 
 def cat(target, genome, file_tree, out_dir):
@@ -81,7 +75,7 @@ def cat(target, genome, file_tree, out_dir):
 
 def load_db(target, genome, tmp_file, out_dir):
     df = pd.read_csv(tmp_file, index_col=0, names=["AugustusAlignmentId", "AlignmentId", "AlignmentIdentity", 
-                                                   "AlignmentCoverage", "CdsAlignmentIdentity", "CdsAlignmentCoverage"])
+                                                   "AlignmentCoverage"])
     df = df.convert_objects(convert_numeric=True)  # have to convert to float because pandas lacks a good dtype function
     df = df.sort_index()
     database_path = os.path.join(out_dir, "augustus_attributes.db")
@@ -92,15 +86,15 @@ def load_db(target, genome, tmp_file, out_dir):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--genome", required=True)
-    parser.add_argument("--refFasta", required=True)
-    parser.add_argument("--targetFasta", required=True)
-    parser.add_argument("--refGp", required=True)
-    parser.add_argument("--augGp", required=True)
+    parser.add_argument("--refTranscriptFasta", required=True)
+    parser.add_argument("--targetTranscriptFasta", required=True)
+    parser.add_argument("--targetTranscriptFastaIndex", required=True)
     parser.add_argument("--outDir", required=True)
     Stack.addJobTreeOptions(parser)
     args = parser.parse_args()
-    i = Stack(Target.makeTargetFn(align_augustus, args=[args.genome, args.refFasta, args.targetFasta, args.refGp,
-                                                        args.augGp, args.outDir])).startJobTree(args)
+    i = Stack(Target.makeTargetFn(align_augustus, args=[args.genome, args.refTranscriptFasta, 
+                                                        args.targetTranscriptFasta, args.targetTranscriptFastaIndex,
+                                                        args.outDir])).startJobTree(args)
     if i != 0:
         raise RuntimeError("Got failed jobs")
 
--- scripts/tmp.py
+++ scripts/tmp.py
@@ -6,9 +6,21 @@ import lib.psl_lib as psl_lib
 
 gp = "/hive/groups/recon/projs/gorilla_eichler/pipeline_data/comparative/susie_3_1/transMap/2015-10-06/transMap/gorilla/transMapGencodeBasicV23.gp"
 ref_gp = "/hive/groups/recon/projs/gorilla_eichler/pipeline_data/comparative/susie_3_1/transMap/2015-10-06/data/wgEncodeGencodeBasicV23.gp"
+aug_gp = "/hive/groups/recon/projs/gorilla_eichler/pipeline_data/comparative/susie_3_1/augustus/tmr/gorilla.output.gp"
 aln_psl = "/hive/groups/recon/projs/gorilla_eichler/pipeline_data/comparative/susie_3_1/transMap/2015-10-06/transMap/gorilla/transMapGencodeBasicV23.psl"
+ref_fasta = "/hive/groups/recon/projs/gorilla_eichler/pipeline_data/assemblies/susie_3_1/human.fa"
+target_fasta = "/hive/groups/recon/projs/gorilla_eichler/pipeline_data/assemblies/susie_3_1/gorilla.fa"
+
 tx_dict = seq_lib.get_transcript_dict(gp)
 ref_dict = seq_lib.get_transcript_dict(ref_gp)
+aug_dict = seq_lib.get_transcript_dict(aug_gp)
 aln_dict = psl_lib.get_alignment_dict(aln_psl)
 
 con, cur = sql_lib.attach_databases("/hive/groups/recon/projs/gorilla_eichler/pipeline_data/comparative/susie_3_1/comparativeAnnotation/2015-10-12/GencodeBasicV23", mode="transMap")
+
+
+aln_id = 'ENST00000340001.8-1'
+aln = aln_dict[aln_id]
+t = tx_dict[aln_id]
+a = ref_dict[aln_id[:-2]]
+
