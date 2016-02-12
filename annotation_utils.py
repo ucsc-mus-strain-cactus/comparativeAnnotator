c1a1547a6076784b8cfe8a589fe47d1e30113e88
ifiddes <ian.t.fiddes@gmail.com>
2016-02-11 17:46:49 -0800

    first commit for rewriting pipeline for luigi, clearing out everything because removing submodules is a pain.

---
+++ annotation_utils.py
@@ -0,0 +1,223 @@
+"""
+This file contains helper functions for comparativeAnnotator.
+"""
+from pycbio.bio.transcripts import GenePredTranscript, find_offset
+
+
+__author__ = "Ian Fiddes"
+
+short_intron_size = 30
+short_cds_size = 75
+
+
+def short_cds(t):
+    """
+    Many classifiers do not apply to CDS below a cutoff size.
+    """
+    return True if t.cds_size <= short_cds_size else False
+
+
+def short_intron(intron):
+    """
+    Many classifiers rely on analyzing introns either above or below a cutoff size
+    """
+    return True if len(intron) <= short_intron_size else False
+
+
+def is_cds(intron, t):
+    return not (intron.start >= t.thick_start and intron.stop < t.thick_stop)
+
+
+def is_not_cds(intron, t):
+    return intron.start >= t.thick_start and intron.stop < t.thick_stop
+
+
+def analyze_intron_gap(t, intron, seq_dict, cds_fn, skip_n, mult3):
+    if skip_n is True and "N" in intron.get_sequence(seq_dict):
+        return False
+    elif skip_n is False and "N" not in intron.get_sequence(seq_dict):
+        return False
+    elif cds_fn(intron, t) is True:
+        return False
+    elif mult3 is True and len(intron) % 3 != 0:
+        return False
+    elif mult3 is False and len(intron) % 3 == 0:
+        return False
+    elif short_intron(intron):
+        return True
+    else:
+        return False
+
+
+def analyze_splice(intron, t, seq_dict, cds_fn, splice_sites):
+    if short_intron(intron) is True:
+        return False
+    seq = intron.get_sequence(seq_dict, strand=True)
+    donor, acceptor = seq[:2], seq[-2:]
+    if cds_fn(intron, t) is True:
+        return False
+    elif "N" in donor or "N" in acceptor:
+        return False
+    elif donor not in splice_sites or splice_sites[donor] != acceptor:
+        return True
+
+
+def insertion_iterator(a, aln, mult3=None):
+    """
+    Target insertion:
+    query:   AATTAT--GCATGGA
+    target:  AATTATAAGCATGGA
+
+    Analyze a given annotation transcript and alignment for target insertions.
+
+    mult3 controls whether only multiple of 3 or only not multiple of 3 are reported. Set to None to report all.
+    """
+    prev_target_i = None
+    exon_starts = [x.start for x in a.exons]
+    for query_i in xrange(len(a)):
+        if query_i in exon_starts:
+            # don't call a intron an insertion
+            prev_target_i = None
+            continue
+        target_i = aln.query_coordinate_to_target(query_i)
+        if target_i is None:
+            # deletion; ignore
+            prev_target_i = target_i
+            continue
+        if prev_target_i is not None and abs(target_i - prev_target_i) != 1:
+            # jumped over a insertion
+            insert_size = abs(target_i - prev_target_i) - 1
+            start = min(prev_target_i, target_i) + 1
+            stop = max(prev_target_i, target_i)
+            if mult3 is True and insert_size % 3 == 0:
+                yield start, stop, insert_size
+            elif mult3 is False and insert_size % 3 != 0:
+                yield start, stop, insert_size
+            elif mult3 is None:
+                yield start, stop, insert_size
+        prev_target_i = target_i
+
+
+def deletion_iterator(t, aln, mult3=None):
+    """
+    Target deletion:
+    query:   AATTATAAGCATGGA
+    target:  AATTAT--GCATGGA
+
+    Analyze a given transcript and alignment for target deletions.
+
+    mult3 controls whether only multiple of 3 or only not multiple of 3 are reported. Set to None to report all.
+    """
+    prev_query_i = None
+    for target_i in xrange(len(t)):
+        target_chrom_i = t.transcript_coordinate_to_chromosome(target_i)
+        query_i = aln.target_coordinate_to_query(target_chrom_i)
+        if query_i is None:
+            # insertion; ignore
+            prev_query_i = query_i
+            continue
+        if prev_query_i is not None and abs(query_i - prev_query_i) != 1:
+            # jumped over a deletion
+            delete_size = abs(query_i - prev_query_i) - 1
+            if t.strand is True:
+                start = stop = target_chrom_i - 1
+            else:
+                start = stop = target_chrom_i + 1
+            if mult3 is True and delete_size % 3 == 0:
+                yield start, stop, -delete_size
+            elif mult3 is False and delete_size % 3 != 0:
+                yield start, stop, -delete_size
+            elif mult3 is None:
+                yield start, stop, -delete_size
+        prev_query_i = query_i
+
+
+def start_out_of_frame(t):
+    """
+    Determines if a start is out of frame by making use of the exonFrames field in a genePred.
+    """
+    # can't use this classifier on BED records due to missing information
+    assert isinstance(t, GenePredTranscript)
+    t_frames = [x for x in t.exon_frames if x != -1]
+    return True if t.strand is True and t_frames[0] != 0 or t.strand is False and t_frames[-1] != 0 else False
+
+
+def frame_shift_iterator(a, t, aln):
+    """
+    Yields frameshift-causing mutations. These are defined as non mult3 indels within CDS.
+    """
+    deletions = list(deletion_iterator(t, aln, mult3=False))
+    insertions = list(insertion_iterator(a, aln, mult3=False))
+    # need to fix case where indel immediately precedes indel by merging overlapping ranges and combining spans
+    merged = []
+    for higher in sorted(deletions + insertions):
+        if not merged:
+            merged.append(higher)
+        else:
+            lower = merged[-1]
+            if higher[0] <= lower[1]:
+                upper_bound = max(lower[1], higher[1])
+                merged[-1] = (lower[0], upper_bound, merged[-1][2] + higher[2])
+            else:
+                merged.append(higher)
+    if t.strand is False:
+        merged = [[y, x, z] for x, y, z in reversed(merged) if z % 3 != 0]
+    else:
+        merged = [[x, y, z] for x, y, z in merged if z % 3 != 0]
+    for start, stop, span in merged:
+        if start >= t.thick_start and stop < t.thick_stop:
+            yield start, stop, span
+
+
+def codon_pair_iterator(a, t, aln, target_seq_dict, query_seq_dict):
+    """
+    Inputs:
+    Transcript objects representing the annotation (query) transcript and the target transcript.
+    PslRow object that represents the alignment between the transcript objects.
+    pyfaidx Fasta objects that contain the genomic sequence for these two transcripts
+
+    Order is (target_cds_pos, target, query)
+    """
+    target_cds = t.get_cds(target_seq_dict).upper()
+    query_cds = a.get_cds(query_seq_dict).upper()
+    a_frames = [x for x in a.exon_frames if x != -1]
+    a_offset = find_offset(a_frames, a.strand)
+    for i in xrange(a_offset, a.cds_size - a.cds_size % 3, 3):
+        target_cds_positions = [t.chromosome_coordinate_to_cds(
+                                aln.query_coordinate_to_target(
+                                a.cds_coordinate_to_transcript(j)))
+                                for j in xrange(i, i + 3)]
+        if None in target_cds_positions:
+            continue
+        # sanity check - should probably remove. But should probably write tests too...
+        assert all([target_cds_positions[2] - target_cds_positions[1] == 1, target_cds_positions[1] -
+                    target_cds_positions[0] == 1, target_cds_positions[2] - target_cds_positions[0] == 2])
+        target_codon = target_cds[target_cds_positions[0]:target_cds_positions[0] + 3]
+        query_codon = query_cds[i:i + 3]
+        assert len(target_codon) == len(query_codon) == 3, a.name
+        yield target_cds_positions[0], target_codon, query_codon
+
+
+def get_adjusted_starts_ends(t, aln):
+    return [[aln.target_coordinate_to_query(intron.start - 1), aln.target_coordinate_to_query(intron.stop)]
+            for intron in t.intron_intervals]
+
+
+def is_fuzzy_intron(intron, aln, ref_starts, fuzz_distance=5):
+    q_gap_start = aln.target_coordinate_to_query(intron.start - 1)
+    q_gap_end = aln.target_coordinate_to_query(intron.stop)
+    return query_contains_intron(q_gap_start - fuzz_distance, q_gap_end + fuzz_distance, ref_starts)
+
+
+def query_contains_intron(q_gap_start, q_gap_end, ref_starts):
+    r = [q_gap_start <= ref_gap <= q_gap_end for ref_gap in ref_starts]
+    return True if any(r) else False
+
+
+def fix_ref_q_starts(ref_aln):
+    if ref_aln.strand == "-":
+        ref_starts = [ref_aln.q_size - (ref_aln.q_starts[i] + ref_aln.block_sizes[i]) for i in
+                      xrange(len(ref_aln.q_starts))]
+    else:
+        ref_starts = ref_aln.q_starts
+    return ref_starts
---
+++ sqlite.py
@@ -0,0 +1,195 @@
+def get_biotype_aln_ids(cur, genome, biotype, filter_chroms=None):
+    """
+    Returns a set of aln_ids which are members of the biotype.
+    """
+    query = "SELECT AlignmentId FROM attributes.'{0}' WHERE TranscriptType = '{1}' AND GeneType = '{1}'"
+    if filter_chroms is not None:
+        for filter_chrom in filter_chroms:
+            query += " AND refChrom != '{}'".format(filter_chrom)
+    query = query.format(genome, biotype)
+    return get_query_ids(cur, query)
+
+
+def get_biotype_ids(cur, ref_genome, biotype, mode="Transcript", filter_chroms=None):
+    """
+    Returns a set of transcript_ids which are members of the biotype. Category should be Transcript or Gene.
+    In gene mode returns the gene_id instead of transcript_id.
+    """
+    assert mode in ["Transcript", "Gene"]
+    query = ("SELECT {0}Id FROM main.'{1}' JOIN attributes.'{1}' USING (TranscriptId) WHERE "
+             "TranscriptType = '{2}' AND GeneType = '{2}'")
+    if filter_chroms is not None:
+        for filter_chrom in filter_chroms:
+            query += " AND refChrom != '{}'".format(filter_chrom)
+    query = query.format(mode, ref_genome, biotype)
+    return get_query_ids(cur, query)
+
+
+def get_all_biotypes(cur, ref_genome, gene_level=False):
+    """
+    Returns all biotypes in the attribute database.
+    """
+    r = "Gene" if gene_level else "Transcript"
+    query = "SELECT {}Type FROM attributes.'{}'".format(r, ref_genome)
+    return get_query_ids(cur, query)
+
+
+def get_ids_by_chromosome(cur, genome, chromosomes=("Y", "chrY")):
+    """
+    Returns all AlignmentIds in the attribute database whose source chromosome is in chromomsomes
+    """
+    base_query = "SELECT AlignmentId FROM attributes.'{}' WHERE {}"
+    filt = ["SourceChrom = '{}'".format(x) for x in chromosomes]
+    filt = " OR ".join(filt)
+    query = base_query.format(genome, filt)
+    return get_query_ids(cur, query)
+
+
+def get_transcript_gene_map(cur, ref_genome, biotype=None, filter_chroms=None):
+    """
+    Returns a dictionary mapping transcript IDs to gene IDs
+    """
+    if biotype is None:
+        query = "SELECT transcriptId,geneId FROM attributes.'{0}'"
+    else:
+        query = "SELECT transcriptId,geneId FROM attributes.'{0}' WHERE transcriptType = '{1}' AND geneType = '{1}'"
+    if filter_chroms is not None:
+        add = "AND".join([" refChrom != '{}' ".format(filter_chrom) for filter_chrom in filter_chroms])
+        if biotype is None:
+            query += " WHERE "
+        else:
+            query += " AND "
+        query += add
+    query = query.format(ref_genome, biotype)
+    return get_query_dict(cur, query)
+
+
+def get_gene_transcript_map(cur, ref_genome, biotype=None, filter_chroms=None):
+    """
+    Returns a dictionary mapping all gene IDs to their respective transcripts
+    """
+    if biotype is None:
+        query = "SELECT geneId,transcriptId FROM attributes.'{0}'"
+    else:
+        query = "SELECT geneId,transcriptId FROM attributes.'{0}' WHERE transcriptType = '{1}' AND geneType = '{1}'"
+    if filter_chroms is not None:
+        add = "AND".join([" refChrom != '{}' ".format(filter_chrom) for filter_chrom in filter_chroms])
+        if biotype is None:
+            query += " WHERE "
+        else:
+            query += " AND "
+        query += add
+    query = query.format(ref_genome, biotype)
+    return get_non_unique_query_dict(cur, query)
+
+
+def get_gene_biotype_map(cur, ref_genome):
+    """
+    Returns a dictionary mapping all gene IDs to their respective biotypes
+    """
+    query = "SELECT geneId,geneType FROM attributes.'{}'".format(ref_genome)
+    return get_query_dict(cur, query)
+
+
+def get_transcript_biotype_map(cur, ref_genome):
+    """
+    Returns a dictionary mapping all transcript IDs to their respective biotypes
+    """
+    query = "SELECT transcriptId,transcriptType FROM attributes.'{}'".format(ref_genome)
+    return get_query_dict(cur, query)
+
+
+def get_fail_passing_excel_ids(cur, ref_genome, genome, biotype, best_cov_only=True, filter_chroms=None,
+                           highest_cov_dict=None):
+    """
+    Returns the IDs categorized as fail, passing_specific, excellent. You can set the best_cov_only flag to only report
+    those transcripts with highest coverage. You can also excellent a premade highest_cov_dict to save computation time.
+    """
+    passing_query = etc.config.transMapEval(ref_genome, genome, biotype, passing=True)
+    excellent_query = etc.config.transMapEval(ref_genome, genome, biotype, passing=False)
+    if best_cov_only is True:
+        if highest_cov_dict is None:
+            best_covs = highest_cov_aln(cur, genome)
+        else:
+            best_covs = highest_cov_dict[genome]
+        best_ids = set(zip(*best_covs.itervalues())[0])
+    else:
+        class Universe:
+            def __contains__(_, __):
+                return True
+        best_ids = Universe()
+    passing_ids = {x for x in get_query_ids(cur, passing_query) if x in best_ids}
+    excellent_ids = {x for x in get_query_ids(cur, excellent_query) if x in best_ids}
+    all_ids = {x for x in get_biotype_aln_ids(cur, genome, biotype) if x in best_ids}
+    fail_ids = all_ids - (passing_ids | excellent_ids)
+    passing_specific_ids = passing_ids - excellent_ids
+    return fail_ids, passing_specific_ids, excellent_ids
+
+
+def collapse_details_dict(details_dict):
+    """
+    Collapses a details dict into a string so that the database record is a string. Properly accounts for the three
+    possibilities: the record is a single BED entry (a string), the record is a list of BED entries, or the record
+    is empty but has been accessed (because the dict is a defaultdict)
+    """
+    collapsed = {}
+    for aln_id, rec in details_dict.iteritems():
+        if len(rec) == 0:  # to deal with the downsides of defaultdict
+            continue
+        if isinstance(rec[0], list):  # hack to determine if this is a list of lists
+            rec = "\n".join(["\t".join(map(str, x)) for x in rec])
+        else:
+            rec = "\t".join(map(str, rec))
+        collapsed[aln_id] = "".join([rec, "\n"])
+    return collapsed
+
+
+def run_transmap_eval(cur, genome, biotype, trans_map_eval, passing=True):
+    """
+    Convenience wrapper for getting all Pass/Excellent transcripts for transMap
+    """
+    query = trans_map_eval(genome, biotype, passing)
+    return get_query_ids(cur, query)
+
+
+def get_stats(cur, genome, mode="transMap", filter_chroms=None):
+    """
+    Returns a dictionary mapping each aln_id to [aln_id, %ID, %COV].
+    We replace all NULL values with zero to make things easier in consensus finding.
+    If filter_chroms is set, will filter out transcripts that come from those transcripts. However, at this point,
+    if this is done in augustus mode nothing will happen.
+    """
+    if mode == "transMap":
+        query = ("SELECT AlignmentId,IFNULL(AlignmentCoverage, 0),IFNULL(AlignmentIdentity, 0) "
+                 "FROM attributes.'{}'")
+        if filter_chroms is not None:
+            add = "AND".join([" sourceChrom != '{}' ".format(filter_chrom) for filter_chrom in filter_chroms])
+            query += " WHERE "
+            query += add
+    else:
+        query = ("SELECT AugustusAlignmentId,IFNULL(AlignmentCoverage, 0),IFNULL(AlignmentIdentity, 0) "
+                 "FROM augustus_attributes.'{}'")
+    return get_query_dict(cur, query.format(genome))
+
+
+def highest_cov_aln(cur, genome, filter_chroms=None):
+    """
+    Returns the set of alignment IDs that represent the best alignment for each source transcript (that mapped over)
+    Best is defined as highest %COV. Also reports the associated coverage and identity values.
+    """
+    tm_stats = get_stats(cur, genome, mode="transMap", filter_chroms=filter_chroms)
+    combined_covs = defaultdict(list)
+    for aln_id, (cov, ident) in tm_stats.iteritems():
+        tx_id = psl_lib.strip_alignment_numbers(aln_id)
+        combined_covs[tx_id].append([aln_id, cov, ident])
+    best_cov = {}
+    for tx_id, vals in combined_covs.iteritems():
+        best_cov[tx_id] = sorted(vals, key=lambda x: [-x[1], -x[2]])[0]
+    return best_cov
+
+
+def get_highest_cov_alns(cur, genomes, filter_chroms=None):
+    """
+    Dictionary mapping each genome to a dictionary reporting each highest coverage alignment and its metrics
+    """
+    return {genome: highest_cov_aln(cur, genome, filter_chroms=filter_chroms) for genome in genomes}
