47cc59022add61020a67d77e2a1cbb40ad1ff2ad
Ian Fiddes <ian.t.fiddes@gmail.com>
2016-07-08 09:20:22 +0100

    Unique IDs script now produces a info file for genePred annotations.

--- scripts/generate_unique_ids.py
+++ scripts/generate_unique_ids.py
@@ -14,9 +14,10 @@ strain IDs are truncated at 10 characters, and have no underscores, but the last
 import argparse
 import sys
 import os
+import re
 os.environ['PYTHONPATH'] = './:./submodules:./submodules/pycbio:./submodules/comparativeAnnotator'
 sys.path.extend(['./', './submodules', './submodules/pycbio', './submodules/comparativeAnnotator'])
-from comparativeAnnotator.database_queries import get_transcript_biotype_map
+from comparativeAnnotator.database_queries import get_transcript_biotype_map, get_gene_biotype_map
 
 
 id_template = 'MGP_{strain_id:.9}{strain_suffix}_{tag_type}{unique_id:07d}.1'
@@ -28,26 +29,30 @@ def parse_args():
     parser.add_argument('genome', help='strain name')
     parser.add_argument('comp_db', help='comparative annotator database')
     parser.add_argument('out_gtf', help='Output GTF', type=argparse.FileType('w'))
+    parser.add_argument('out_table', help='Output data table', type=argparse.FileType('w'))
     return parser.parse_args()
 
 
 def main():
     args = parse_args()
     transcript_biotype_map = get_transcript_biotype_map('C57B6J', args.comp_db)
+    gene_biotype_map = get_gene_biotype_map('C57B6J', args.comp_db)
     gene_counter = 1
     transcript_counter = 1
     seen_genes = {}
     seen_transcripts = {}
     genome = args.genome.replace('_', '')[:-1]
-    assert genome.isalnum()
+    assert genome.isalnum(), genome
     genome_suffix = args.genome[-1]
+    tx_table = {}
     for line in args.gtf:
         line = line.rstrip().split('\t')
         attrs = dict([x.split(' ') for x in line[-1].replace('"', '').split('; ')])
         attrs['parent_gene_id'] = attrs['gene_id']
         attrs['parent_transcript_id'] = attrs['transcript_id']
         # we assume protein coding for CGP transcripts
-        attrs['biotype'] = transcript_biotype_map.get(attrs['transcript_id'], 'protein_coding')
+        attrs['transcript_biotype'] = transcript_biotype_map.get(attrs['transcript_id'], 'unknown_likely_coding')
+        attrs['gene_biotype'] = gene_biotype_map.get(attrs['gene_id'], 'unknown_likely_coding')
         if attrs['parent_gene_id'] not in seen_genes:
             seen_genes[attrs['parent_gene_id']] = gene_counter
             gene_counter += 1
@@ -63,9 +68,17 @@ def main():
                                                   unique_id=seen_transcripts[attrs['parent_transcript_id']])
             attrs['exon_id'] += '.' + attrs['exon_number']
         # rebuild the line
-        attributes = '; '.join([' '.join([x, '"' + y + '"']) for x, y in attrs.iteritems()])
+        attributes = '; '.join([' '.join([x, '"' + y + '"']) for x, y in attrs.iteritems()]) + ';'
         line[-1] = attributes
+        if line[2] == 'transcript':
+            tx_table[attrs['transcript_id']] = [attrs['transcript_id'], attrs['parent_transcript_id'],
+                                                attrs['gene_id'], attrs['parent_gene_id'],
+                                                attrs['gene_biotype'], attrs['transcript_biotype']]
         args.out_gtf.write('\t'.join(line) + '\n')
+    args.out_table.write('\t'.join(['#transcript_id', 'parent_transcript_id', 'gene_id', 'parent_gene_id',
+                                    'gene_biotype', 'transcript_biotype']) + '\n')
+    for rec in tx_table.itervalues():
+        args.out_table.write('\t'.join(rec) + '\n')
 
 
 if __name__ == '__main__':
