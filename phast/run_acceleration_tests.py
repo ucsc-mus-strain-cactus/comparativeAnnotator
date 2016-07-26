9062c58bb7d625b2b50eb07b23b01af6bf73e5e3
ifiddes <ian.t.fiddes@gmail.com>
2016-07-26 13:28:10 -0700

    cleaned up code, added logging

--- phast/run_acceleration_tests.py
+++ phast/run_acceleration_tests.py
@@ -9,7 +9,7 @@ from ete3 import Tree
 from collections import OrderedDict
 from jobTree.scriptTree.target import Target
 from jobTree.scriptTree.stack import Stack
-from sonLib.bioio import system, TempFileTree, popenCatch
+from sonLib.bioio import system, TempFileTree, popenCatch, setLoggingFromOptions, logger
 
 
 def parse_args():
@@ -38,6 +38,17 @@ def format_ratio(numerator, denominator):
     if denominator == 0:
         return float("nan")
     return float(numerator) / denominator
+
+
+def grouper(iterable, size):
+    """
+    http://stackoverflow.com/questions/434287/what-is-the-most-pythonic-way-to-iterate-over-a-list-in-chunks
+    """
+    it = iter(iterable)
+    chunk = tuple(itertools.islice(it, size))
+    while chunk:
+        yield chunk
+        chunk = tuple(itertools.islice(it, size))
 
 
 def rename_model(target, model, accelerated_genomes):
@@ -80,60 +91,87 @@ def extract_maf_wrapper(target, args):
     """
     Main pipeline wrapper. Calls out to hal2maf once for each region in args.conserved_bed
     """
+    accelerated_genomes = set(args.accelerated_genomes + [args.ref_genome])
+    outgroup_genomes = set(args.target_genomes) - accelerated_genomes
     bed_recs = [x.split() for x in open(args.conserved_bed)]
     result_dir = target.getGlobalTempDir()
     result_tree = TempFileTree(result_dir)
-    for chrom, start, stop, score in bed_recs:
-        if float(score) <= args.single_copy_percent_cutoff:
-            continue
-        result_path = result_tree.getTempFile(suffix='_' + '_'.join([chrom, start, stop]))
-        target.addChildTargetFn(extract_and_calculate, args=(args, chrom, start, stop, result_path))
+    for chunk in grouper(bed_recs, 50):
+        result_path = result_tree.getTempFile()
+        target.addChildTargetFn(extract_and_calculate, args=(args, chunk, accelerated_genomes, outgroup_genomes, result_path))
     target.setFollowOnTargetFn(cat_results, args=(args, result_tree.listFiles()))
 
 
-def extract_and_calculate(target, args, chrom, start, stop, result_path):
-    """
-    Runs the fitting, testing each ancestor that is under accelerated_genomes
-    """
-    # extract region MAF
-    accelerated_genomes = set(args.accelerated_genomes + [args.ref_genome])
-    outgroup_genomes = set(args.target_genomes) - accelerated_genomes
+def extract_conserved_region(target, args, chrom, start, stop):
+    """extract maf for a specific region and the target genomes"""
     maf_path = os.path.join(target.getGlobalTempDir(), 'region.maf')
     size = int(stop) - int(start)
     cmd = 'hal2maf --noAncestors --targetGenomes {} --refGenome {} --refSequence {} --start {} --length {} {} {}'
     cmd = cmd.format(','.join(args.target_genomes), args.ref_genome, chrom, start, size, args.hal, maf_path)
     system(cmd)
-    # write the region BED needed by phastOdds
+    target.logToMaster('extracted MAF for region {}:{}-{}'.format(chrom, start, stop))
+    return maf_path
+
+
+def write_region_bed(target, chrom, start, stop):
     region_bed = os.path.join(target.getGlobalTempDir(), 'region.bed')
     with open(region_bed, 'w') as outf:
         outf.write('\t'.join([chrom, start, stop]) + '\n')
-    # calculate the initial region-specific conserved model
+    return region_bed
+
+
+def calculate_region_specific_model(target, args, maf_path):
+    """calculate the initial region-specific conserved model"""
     # phyloFit writes to the file *.mod in whatever the path set by --out-root is
     region_specific_conserved = os.path.join(target.getGlobalTempDir(), 'region_specific_conserved')
     cmd = 'phyloFit --init-model {} --scale-only --out-root {} {}'
     cmd = cmd.format(args.conserved_model, region_specific_conserved, maf_path)
     system(cmd)
     region_specific_conserved += '.mod'
-    # should we continue?
-    if conserved_model_contains_sufficient_outgroups(region_specific_conserved, accelerated_genomes, outgroup_genomes):
-        # for each ancestral sequence, test all ancestral sequences
-        with open(result_path, 'w') as outf:
-            for anc_name, branch_model in rename_model(target, region_specific_conserved, accelerated_genomes):
-                region_specific_accelerated = os.path.join(target.getGlobalTempDir(), 'region_specific_accelerated')
-                cmd = 'phyloFit --init-model {} --scale-subtree {}:loss --out-root {} {}'
-                cmd = cmd.format(branch_model, anc_name, region_specific_accelerated, maf_path)
-                region_specific_accelerated += '.mod'
-                system(cmd)
-                cmd = 'phastOdds --output-bed --features {} --background-mods {} --feature-mods {} {}'
-                cmd = cmd.format(region_bed, branch_model, region_specific_accelerated, maf_path, result_path)
-                r = popenCatch(cmd)
-                l = r.split()
-                # discard the result if the test is not positive
-                if int(l[-1]) > 0:
-                    l[-2] = anc_name
-                    outf.write('\t'.join(l) + '\n')
-    else:
-        open(result_path, 'w').close()
+    return region_specific_conserved
+
+
+def test_ancestral_nodes(target, region_specific_conserved, accelerated_genomes, maf_path, region_bed, outf_handle):
+    """Run LRT test on each ancestral node below the common ancestor of the accelerated genomes"""
+    for anc_name, branch_model in rename_model(target, region_specific_conserved, accelerated_genomes):
+        region_specific_accelerated = os.path.join(target.getGlobalTempDir(), 'region_specific_accelerated')
+        cmd = 'phyloFit --init-model {} --scale-subtree {}:loss --out-root {} {}'
+        cmd = cmd.format(branch_model, anc_name, region_specific_accelerated, maf_path)
+        region_specific_accelerated += '.mod'
+        system(cmd)
+        cmd = 'phastOdds --output-bed --features {} --background-mods {} --feature-mods {} {}'
+        cmd = cmd.format(region_bed, branch_model, region_specific_accelerated, maf_path)
+        r = popenCatch(cmd)
+        l = r.split()
+        # discard the result if the test is not positive
+        if int(l[-1]) > 0:
+            l[-2] = anc_name
+            outf_handle.write('\t'.join(l) + '\n')
+
+
+def extract_and_calculate(target, args, chunk, accelerated_genomes, outgroup_genomes, result_path):
+    """
+    Runs the fitting, testing each ancestor that is under accelerated_genomes
+    """
+    with open(result_path, 'w') as outf_handle:
+        for chrom, start, stop, score in chunk:
+            # do not test a region if it is not at least single_copy_percent_cutoff
+            if float(score) <= args.single_copy_percent_cutoff:
+                target.logToMaster('locus {}:{}-{} has insufficient single copy percentage {}'.format(chrom, start, stop, score))
+                continue
+            # extract region MAF
+            maf_path = extract_conserved_region(target, args, chrom, start, stop)
+            # write the region BED needed by phastOdds
+            region_bed = write_region_bed(target, chrom, start, stop)
+            # calculate the initial region-specific conserved model
+            region_specific_conserved = calculate_region_specific_model(target, args, maf_path)
+            # should we continue?
+            if not conserved_model_contains_sufficient_outgroups(region_specific_conserved, accelerated_genomes,
+                                                                 outgroup_genomes):
+                target.logToMaster('locus {}:{}-{} has insufficient outgroups'.format(chrom, start, stop))
+                continue
+            test_ancestral_nodes(target, region_specific_conserved, accelerated_genomes, maf_path, region_bed,
+                                 outf_handle)
 
 
 def cat_results(target, args, paths):
@@ -153,6 +191,7 @@ def cat_results(target, args, paths):
 if __name__ == '__main__':
     from phast.run_acceleration_tests import *
     args = parse_args()
+    setLoggingFromOptions(args)
     s = Stack(Target.makeTargetFn(extract_maf_wrapper, args=(args,)))
     i = s.startJobTree(args)
     if i != 0:
